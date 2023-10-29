fe376e415b8f8640dd21744f273205414e38d8d7
fadihanna123 <54924938+fadihanna123@users.noreply.github.com>
2023-10-29 15:23:48 +0100

    Reconfig Eslint and add more strict tsconfig rules

--- Dashboard/Js/main.js
+++ Dashboard/Js/main.js
@@ -1,28 +1,23 @@
 // @ts-check
 // Implementerar Bootstrapvalidering i formulär.
-(() => {
+(function () {
   window.addEventListener(
     'load',
-    () => {
-      const forms = document.getElementsByClassName('needs-validation');
-      Array.prototype.filter.call(
-        forms,
-        (
-          /** @type {{ addEventListener: (arg0: string, arg1: (event: any) => void, arg2: boolean) => void; checkValidity: () => boolean; classList: { add: (arg0: string) => void; }; }} */ form
-        ) => {
-          form.addEventListener(
-            'submit',
-            (event) => {
-              if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-              }
-              form.classList.add('was-validated');
-            },
-            false
-          );
-        }
-      );
+    function () {
+      var forms = document.getElementsByClassName('needs-validation');
+      Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          'submit',
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          },
+          false
+        );
+      });
     },
     false
   );
--- Js/main.js
+++ Js/main.js
@@ -1,21 +1,13 @@
-// @ts-check
-// Implementerar kontroll om lösenordet är mindre än 6 tecken.
 $(document).on('ready', () => {
     $('#registerpsw').on('click', () => {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
         if ($('#registerpsw').val()[0].length >= 6) {
-            // Om lösenordets längd var längre än 6 tecken.
             $('#registerpsw').css('border-color', '#008000');
         }
         else {
-            // Slut om lösenordets längd var längre än 6 tecken.
-            // Om lösenordets längd var mindre än 6 tecken.
             $('#registerpsw').css('border-color', 'red');
-        } // Slut om lösenordets längd var mindre än 6 tecken.
+        }
     });
 });
-// Implementerar Bootstrapvalidering i formulären.
 (() => {
     window.addEventListener('load', () => {
         const forms = document.getElementsByClassName('needs-validation');
@@ -30,3 +22,4 @@ $(document).on('ready', () => {
         });
     }, false);
 })();
+export {};
