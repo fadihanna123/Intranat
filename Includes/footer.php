d0a5efc688526ffc377299dd32344981cee72ff6
Fadi Hanna <54924938+fadihanna123@users.noreply.github.com>
2024-05-01 19:11:06 +0200

    Remove useless space in footer.php

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,10 +1,11 @@
   <!-- Sidfoten -->
- <div class="container-fluid">
+  <div class="container-fluid">
     <footer class="jumbotron bg-light h-25 text-center" id="sidfoten">
-        <?php echo $getname; ?><br />
-        &copy;  Copyright 2023 <?php echo $getname; ?>. 
-    <br />
+      <?php echo $getname; ?><br />
+      &copy; Copyright 2023 <?php echo $getname; ?>.
+      <br />
     </footer>
   </div>
-</body>
-</html>
+  </body>
+
+  </html>
