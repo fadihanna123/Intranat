9453e5d734416db9c1242f5a4cc7e356b0937fb1
Fadi Hanna <fadialias230@gmail.com>
2025-09-04 21:56:22 +0200

    Upgrade packages

--- Includes/config.php
+++ Includes/config.php
@@ -2,13 +2,13 @@
 
 error_reporting(-1);
 ini_set("display_errors", 1);
-error_reporting(E_ALL | E_STRICT);
+error_reporting(E_ALL);
 
 spl_autoload_register(function ($class) {
   include 'Classes/' . $class . '.class.php';
 });
 
-define("DB_HOST", "localhost");
+define("DB_HOST", "127.0.0.1");
 define("DB_USRNAME", "fadi");
 define("DB_PSW", "0000");
 define("DB_NAME", "intranat");
